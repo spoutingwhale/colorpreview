0da4c279904126ee066f04e8bb2fae54d89f36d3
Andrew Linim <holinim@ya.ru>
2026-03-11 21:38:09 +0300

    feat: inline color preview.

--- main.py
+++ main.py
@@ -6,66 +6,118 @@ from aiogram.filters.command import Command
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from os import getenv
-from dotenv import load_dotenv; load_dotenv()
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=getenv("API-KEY"), default=DefaultBotProperties(parse_mode = ParseMode.HTML))
+bot = Bot(
+    token=getenv("API-KEY"), default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 from json import load
 
-with open('utils/strings.json') as file:
+with open("utils/strings.json") as file:
     strings = load(file)
     file.close()
 
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    if message.from_user.language_code not in strings.keys(): code = 'en'
-    else: code = message.from_user.language_code
-    await message.reply(strings[code]['greet'], disable_web_page_preview = True)
+    if message.from_user.language_code not in strings.keys():
+        code = "en"
+    else:
+        code = message.from_user.language_code
+    await message.reply(strings[code]["greet"], disable_web_page_preview=True)
+
 
 @dp.inline_query()
 async def inline(query: types.InlineQuery):
-    if query.from_user.language_code not in strings.keys(): code = 'en'
-    else: code = query.from_user.language_code
+    if query.from_user.language_code not in strings.keys():
+        code = "en"
+    else:
+        code = query.from_user.language_code
     query_text = query.query
+
+    if query_text == "":
+        response = strings[code]["empty"]
+        title = strings[code]["empty"]
+        result = types.InlineQueryResultArticle(
+            id=query.id,
+            title=title,
+            input_message_content=types.InputTextMessageContent(message_text=response),
+        )
+        await bot.answer_inline_query(query.id, results=[result])
+        return
+
     if utils.ishex(query_text) == True:
         if len(query_text) == 7:
             color = query_text[1:]
         else:
             color = query_text
 
-        title = strings[code]['title'].replace("<input>", query_text)
-        response = utils.makeresponse(utils.generate(color), query_text, strings[code]['output'])
-    elif (len(query_text.split(' ')) == 3) and all(utils.isnum(c) for c in query_text.split(' ')) and all(int(c) <= 255 for c in query_text.split(' ')) and all(int(c) >= 0 for c in query_text.split(' ')):
-        rgb = query_text.split(' ')
-        color = '%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+        title = strings[code]["title"].replace("<input>", query_text)
+        response = utils.makeresponse(utils.generate(color), strings[code]["output"])
+    elif (
+        (len(query_text.split(" ")) == 3)
+        and all(utils.isnum(c) for c in query_text.split(" "))
+        and all(int(c) <= 255 for c in query_text.split(" "))
+        and all(int(c) >= 0 for c in query_text.split(" "))
+    ):
+        rgb = query_text.split(" ")
+        color = "%02x%02x%02x" % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
         if utils.ishex(color) == True:
-            title = strings[code]['title'].replace("<input>", query_text)
-            response = utils.makeresponse(utils.generate(color), query_text, strings[code]['output'])
-            strings[code]['title'].replace("<input>", query_text)
+            title = strings[code]["title"].replace("<input>", query_text)
+            response = utils.makeresponse(
+                utils.generate(color),
+                strings[code]["output"],
+            )
         else:
-            response = strings[code]['badcolor']
-            title = strings[code]['badcolor']
+            response = strings[code]["badcolor"]
+            title = strings[code]["badcolor"]
+            result = types.InlineQueryResultArticle(
+                id=query.id,
+                title=title,
+                input_message_content=types.InputTextMessageContent(
+                    message_text=response
+                ),
+            )
+            await bot.answer_inline_query(query.id, results=[result])
+            return
     elif query_text in utils.db.keys():
         color = utils.hexbycolorname(query_text)
-        title = strings[code]['title'].replace("<input>", query_text)
-        response = utils.makeresponse(utils.generate(color), query_text, strings[code]['output'])
+        title = strings[code]["title"].replace("<input>", query_text)
+        response = utils.makeresponse(utils.generate(color), strings[code]["output"])
     else:
-        response = strings[code]['badcolor']
-        title = strings[code]['badcolor']
+        response = strings[code]["badcolor"]
+        title = strings[code]["badcolor"]
+        result = types.InlineQueryResultArticle(
+            id=query.id,
+            title=title,
+            input_message_content=types.InputTextMessageContent(message_text=response),
+        )
+        await bot.answer_inline_query(query.id, results=[result])
+        return
 
-    result = types.InlineQueryResultArticle(
-        id = query.id,
-        title = title,
-        input_message_content = types.InputTextMessageContent(message_text=response)
+    photo_url = strings["url"].replace("{}", color)
+
+    result = types.InlineQueryResultPhoto(
+        id=query.id,
+        photo_url=photo_url,
+        thumbnail_url=photo_url,
+        title=title,
+        caption=response,
+        parse_mode=ParseMode.HTML,
     )
 
     await bot.answer_inline_query(query.id, results=[result])
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- utils/utils.py
+++ utils/utils.py
@@ -1,29 +1,33 @@
 import json
 from colorsys import rgb_to_hls, rgb_to_hsv
 
-fl = open('names.json')
+fl = open("names.json")
 db = json.loads(fl.read())
 fl.close()
 
 del fl
 
+
 def hex2rgb(value: str):
-    value = value.lstrip('#')
+    value = value.lstrip("#")
     lv = len(value)
-    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+    return tuple(int(value[i : i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
 
 def rgb2hex(values):
     r, g, b = values
-    return '%02x%02x%02x' % (r, g, b)
+    return "%02x%02x%02x" % (r, g, b)
+
 
 def hexbycolorname(name: str):
     if name in db.keys():
         rgb = db[name]
-        hex = '%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
+        hex = "%02x%02x%02x" % (rgb[0], rgb[1], rgb[2])
         return hex
     else:
         return False
-    
+
+
 def colornamebyhex(hex: str):
     for i in db.keys():
         rgb = hex2rgb(hex)
@@ -31,10 +35,11 @@ def colornamebyhex(hex: str):
         if db[i] == rgb:
             return i
     return False
-    
+
+
 def nearestcolor(hex: str):
     rgb = hex2rgb(hex)
-    result = ['', 10000, '']
+    result = ["", 10000, ""]
     for i in db.keys():
         diff = 0
         diff += abs(rgb[0] - db[i][0])
@@ -44,8 +49,9 @@ def nearestcolor(hex: str):
             result[0] = i
             result[1] = diff
             result[2] = db[i]
-        
+
     return result, rgb2hex(result[2])
+
 
 def rgb2cmyk(r, g, b):
     if (r, g, b) == (0, 0, 0):
@@ -63,14 +69,18 @@ def rgb2cmyk(r, g, b):
 
     return c * 100, m * 100, y * 100, k * 100
 
+
 def ishex(color):
     if len(color) != 6 or not all(c in "0123456789ABCDEFabcdef" for c in color):
-        if len(color[1:]) != 6 or not all(c in "0123456789ABCDEFabcdef" for c in color[1:]):
+        if len(color[1:]) != 6 or not all(
+            c in "0123456789ABCDEFabcdef" for c in color[1:]
+        ):
             return False
         else:
             return True
     else:
         return True
+
 
 def isnum(num):
     try:
@@ -79,12 +89,14 @@ def isnum(num):
     except ValueError:
         return False
 
+
 def generate(color):
     name, nearestnamedhex = nearestcolor(color)
 
     name = name[0]
 
-    if color != nearestnamedhex: name += f' (#<code>{nearestnamedhex}</code>)'
+    if color != nearestnamedhex:
+        name += f" (#<code>{nearestnamedhex}</code>)"
 
     rgb = hex2rgb(color)
     hsv = rgb_to_hsv(rgb[0], rgb[1], rgb[2])
@@ -100,7 +112,7 @@ def generate(color):
 
     for i in range(len(hls)):
         newhls.append(round(hls[i], 2))
-    
+
     for i in range(len(cmyk)):
         newcmyk.append(round(cmyk[i], 2))
 
@@ -110,12 +122,14 @@ def generate(color):
 
     return [color, name, rgb, hsv, hsl, cmyk]
 
-def makeresponse(values, query_text, template):
+
+def makeresponse(values, template):
     color, name, rgb, hsv, hsl, cmyk = values
-    return template.replace("<input>", query_text)\
-        .replace("<hex>", f"{color}")\
-        .replace("<name>", name)\
-        .replace("<rgb>", f"({rgb[0]}, {rgb[1]}, {rgb[2]})")\
-        .replace("<hsv>", hsv)\
-        .replace("<hsl>", hsl)\
+    return (
+        template.replace("<hex>", f"{color}")
+        .replace("<name>", name)
+        .replace("<rgb>", f"({rgb[0]}, {rgb[1]}, {rgb[2]})")
+        .replace("<hsv>", hsv)
+        .replace("<hsl>", hsl)
         .replace("<cmyk>", cmyk)
+    )
